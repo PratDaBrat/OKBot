754158e53d0c02a994a86407208c06d7735dd5c6
Pratyush Chowdhury <pratyush.chow8@gmail.com>
2020-10-27 10:27:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ OKBot.on('message', message => {
 
 	if(message.content.toLowerCase().includes('ok') == true && !message.author.bot) {
 		rep = message.content.replace('ok','🆗');
-		oc = '       message originally sent by '.concat(message.author.username);
+		oc = '                message originally sent by '.concat(message.author.username);
 		fin = rep.concat(oc);
 		message.delete()
 		message.channel.send(fin);
